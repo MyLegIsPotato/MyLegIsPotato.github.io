3a275b90b2bb596c5a92000040460fec2b8feab0
Mateusz Telakowski <38456590+MyLegIsPotato@users.noreply.github.com>
2020-08-24 12:29:31 +0200

    Add files via upload

---
+++ translation.js
@@ -0,0 +1,107 @@
+var arrLang = {
+		'en' : {
+			'contact' : 'Contact',
+			'portfolio' : 'Portfolio',
+			'specialities' : 'Specialities',
+			'about' : 'About me',
+			'3d' : '3D Modelling',
+			'art' : 'Digital Art',
+			'gamedev' : 'Game Development',
+			'programming' : 'Programming',
+			'websites' : 'Webmastering',
+			'hi' : 'Hi.',
+			'descr' : 'My name is Matt and this is my website.',
+			'aboutText' : 'I\'m Matthew Telakowski. I was born in October 2000 in Poland. Ever since I can remember, I\'ve always loved technology, especially computers. I\'ve played a lot of games too — that may be the thing that started my desire to IT. When I was around 12 I tried looking for youtube videos about the game development. It was a shocker for little, silly and inexperienced me. I gave up back then. Three years of playing games later I felt this again. Now I knew that I want to make my own video game taking into the consideration how hard and brutal the path to this achievement is. But I couldn\'t give up. One more time, this time smarter and not that easily discouraged I searched for Unity tutorials. As it turned I got to learn C# to create my dream game. I just didn\'t want to rely on pieces created by someone else. I wanted to be the creator. So I speed-watched C++ course (as everybody recommended) and then proceed to learn C#. The same year I finished my secondary school and went for IT class in Technical school. That made me make a few more steps towards to my dreams. This school made me learn web technologies like HTML and CSS. Before I thought that this is something that I\'ll never learn just because it looked much boring than programming. I was wrong. It is just a different thing, other experience that I loved. When learning webmastering I was still working on my game dev skills. Writing Unity scripts weren\'t enough for me, I needed 3D models. In summer 2017 I bought a one-month online course to Blender. Software that is so hard that you could smash rocks with it, at least at the beginning. After watching course for 2 weeks straight I managed to deal with every pro and con of it and started creating game assets for my games. Later on I tried creating some 2D digital art, in which I am still beginner. Maybe one day. I also wanted to learn music theory, but I can\'t put so much time in it when I can keep programming and working on my games. In future I\'m planning to finish college and aquire Software Engineer degree.',
+			'phone' : 'Phone: +48 698 770 636',
+			'email' : 'E-mail: mateusztelakowski@outlook.com',
+			'fb' : 'Message me using facebook:',
+			'discord' : 'Add me on discord: Lel Lelovsky#9605 (Quickest respond time!)',
+			'li' : 'Contact me using LinkedIn:',
+			'portfolio_games' : 'Games',
+			'portfolio_renders' : 'Renders',
+			'portfolio_websites' : 'Websites',
+			'portfolio_artworks' : 'Artworks',
+			'portfolio_animations' : 'Animations',
+			'title' : 'IT Technician',
+			'skills' : 'Skills',
+			'q1' : 'Stop comparing yourself with the others. Instead start comparing today\'s self with yesterday\'s self.',
+			'programming' : "Programming",
+			'programmingContent' : 'I\'d started programming in 2015, since then I\'ve used C (for arduino), C++ (for learning), C# (my favorite), Python (for fun), JavaScript (because I had to). Programming gives me the feeling nothing else does, sometimes the work can get hard but it\'s always possible to complete.',
+			'gamedev' : 'Gamedev',
+			'gamedevContent' : 'My first contact with game development software was in 2012, got scared of it as a child. Came back to it 3 years later and it basically lauched me into programming and gave me huge passion to computers in general.',
+			'repairs' : 'Technical Support',
+			'repairsBar' : 'Technical Support (Polish only)',
+			'repairsShort' : 'Support',
+			'repairsContent' : 'I was a go-to fix-it family member since I can remember, I\'ve always got along with machines better than with other humans. Built my first PC in 2013. Solved multiple computer problems for my family and friends, either software or hardware. It is very rare for me to not be able to fix a problem or atleast find an amazing workaround.',
+			'3d' : '3D Rendering',
+			'3dContent' : 'I like doing 3D renders for fun. It\'s so much fun to create something that looks good and use it as your own desktop wallpaper or share it with other people online. I mainly use Blender.',
+			'digitalart' : 'Art',
+			'digitalartContent' : 'Creating \"flat\" art is my hobby too. I don\'t want to stick to one method so I experiment with hand-drawing either on paper or digitally. Nothing helps more in communication that a well-drawn sketch. The other way for me is using photo manipulation to create something new or most likely just funny - like an logo edit.',
+			'programmingPortfolioButton' : 'Programming portfolio',
+			'graphicsPortfolioButton' : 'Graphics portfolio'
+		},
+
+		'pl' : {
+			'contact' : 'Kontakt',
+			'portfolio' : 'Portfolio',
+			'specialities' : 'Umiejętności',
+			'about' : 'O mnie',
+			'3d' : 'Modelowanie 3D',
+			'art' : 'Grafika 2D',
+			'gamedev' : 'Tworzenie gier',
+			'programming' : 'Programowanie',
+			'websites' : 'Webmastering',
+			'hi' : 'Witaj.',
+			'descr' : 'Ja jestem Mateusz, a to jest moja strona.',
+			'aboutText' : 'Nazywam się Mateusz Telakowski, w internecie działam pod pseudonimem Lel Lelovsky i mylegispotato. Urodziłem się w październiku 2000 roku. Pierwszy komputer obsługiwałem prawdopodobnie kiedy miałem 4 lata. Od urodzenia otaczałem się technologią, byłem zafascynowany wszystkimi mechanizmami i gadającymy zabawkami. Każdego dnia jestem zachwycony faktem, że ludzkość dała radę nauczyć kamień myślenia, a teraz nie dałaby rady bez niego funkcjonować. To jak technologia potrafi zmienić nasze życia jest niesamowite, od rozrywki po ratowanie ludzkich istnień. Dlatego właśnie, sam chcę być osobą kreatywną i dołożyć swoich starań w rozwój technologii, stworzyć coś pięknego. W 2020 roku zdobyłem tytuł Technika Informatyka i kontynuując swoją edukację pracuję ze znajomymi nad grą, a także chętnie udzielę Ci wsparcia technicznego. Więcej info niżej.',
+			'phone' : 'Tel.: +48 698 770 636',
+			'email' : 'E-mail: mateusztelakowski@outlook.com',
+			'fb' : 'Napisz do mnie na facebook\'u:',
+			'discord' : 'Dodaj mnie na discordzie: Lel Lelovsky#9605 (Najszybsza odpowiedź!)',
+			'li' : 'Zobacz profil na LinkedIn:',
+			'portfolio_games' : 'Gry',
+			'portfolio_renders' : 'Rendery',
+			'portfolio_websites' : 'Strony Internetowe',
+			'portfolio_artworks' : 'Prace graficzne',
+			'portfolio_animations' : 'Animacje',
+			'title' : 'Technik Informatyk',
+			'skills' : 'Umięjętności',
+			'q1' : 'Nie porównuj się z innymi, a jedynie ze sobą z dnia poprzedniego.',
+			'programming' : "Programowanie",
+			'programmingContent' : 'Programować zacząłem w 2015. Od tego czasu miałem styczność z wieloma językami i technologiami np. C, C++, C#, HTML, CSS, Python, JavaScript, MySQL. C# i .NET są moimi ulubionymi.',
+			'gamedev' : 'Tworzenie gier',
+			'gamedevContent' : 'Pierwszy kontakt z tworzeniem gier miałem w 2012. Poziom skomplikowania pracy mnie przerósł i wróciłem do tego 3 lata później. Od tego czasu zakochałem się w programowaniu i pozostałych procesach twórczych, nawet w tych niezwiązanymi z tworzeniem gier.',
+			'repairs' : 'Wsparcie Techniczne',
+			'repairsShort' : 'Wsparcie',
+			'repairsBar' : 'Wsparcie Techniczne',
+			'repairsContent' : 'Już od dziecka byłem rodzinnym informatykiem. Kiedy pojawiał się problem z komputerem, czy to u kogoś z rodziny, czy u znajomego zawsze pochłaniało mnie to w takim stopniu, że udawało mi się dany problem rozwiązać lub przynajmniej znaleźć sposób na jego obejście. Czasami mam wrażenie, że lepiej dogaduję się z maszynami, niż ludźmi. :)',
+			'3d' : 'Renderowanie 3D',
+			'3dContent' : 'Lubię tworzyć rendery 3D dla zabawy. Bardzo przyjemnie jest ustawić na tło pulpitu własnoręcznie stworzone dzieło. Pracuję głównie w Blenderze. Moje najlepsze dzieła możesz zobaczyć w portfolio na DeviantArt.',
+			'digitalart' : 'Grafika',
+			'digitalartContent' : '\"Płaskie\" grafiki też dają spore pole do popisu kreatywności. Czy to szybki szkic na kartce, czy malowanie przy użyciu tabletu, jedno jest pewne - nic nie przekazuje pomysłu lepiej niż dobra reprezentacja graficzna. Z drugiej strony zamiast tworzyć lubię też przerabiać np. loga znanych firm.',
+			'programmingPortfolioButton' : 'Portfolio programowania',
+			'graphicsPortfolioButton' : 'Portfolio graficzne'
+		},
+	};
+	
+
+	$(function(){
+		
+		if(Cookies.get('language') != 'en' && Cookies.get('language') != 'pl')
+		{
+		Cookies.set('language', 'pl');
+		}
+		
+		var a = Cookies.get('language');
+		
+		var lang = 'en';	
+		$('.lang').each(function(index, element){$(this).text(arrLang[a][$(this).attr('key')])});
+
+		
+		$('.translate').click(function(){
+			a = $(this).attr('id');
+			Cookies.set('language', a);
+			$('.lang').each(function(index, element){$(this).text(arrLang[a][$(this).attr('key')])});
+		});
+		
+	});		
